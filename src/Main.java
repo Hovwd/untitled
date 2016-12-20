a39eca53f269debc47872b6d422f51a2b0873d29
gevorg <gevorg.mushyan@webbfontaine.am>
2016-12-20 17:29:45 +0400

    asa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello enia!");
+        System.out.println("Hello enifxjhfj,uyd,euyr6elu6erl8u6a!");
     }
 }
